21dadd210f7d80825b3885b43fcc88c813d0c9b6
lev <leva.casian@mail.ru>
2025-04-13 22:53:15 +0300

    Updates

---
+++ _nuxt/B9mIltn6.js
@@ -0,0 +1,1 @@
+import{l as t}from"./TjCxX7sJ.js";import{_ as s,v as c,t as l}from"./Cd000Fzs.js";t("ws://localhost:3000").on("hello",o=>{console.log(o)});const n={};function r(e,o,a,p,_,f){return l(),c("h1",null,"тут будут сокеты")}const u=s(n,[["render",r]]);export{u as default};
